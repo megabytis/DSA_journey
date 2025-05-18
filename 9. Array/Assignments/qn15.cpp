c59ae1ae47f4e800c32519d338dd473acc14a013
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-05-18 20:29:47 +0530

    adding kaden's algorithm of gfg

---
+++ 9. Array/Assignments/qn15.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSubArray(vector<int> v) {
+    int currentSum = 0;
+    int maxSum = INT_MIN;
+
+    for(int i = 0; i < v.size(); i++) {
+        currentSum = max(v[i],currentSum + v[i]);
+        maxSum = max(currentSum, maxSum);
+    }
+
+    return maxSum;
+}
+
+int main() {
+    vector<int> v = {-2,-4};
+
+    cout << maxSubArray(v) << endl;
+
+    return 0;
+}
